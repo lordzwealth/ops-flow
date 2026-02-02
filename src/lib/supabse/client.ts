2350d45fd7438434080391e291616d62ddea5da4
Emmanuel Ajayi <ajayiemmanuel215@gmail.com>
2026-02-02 09:34:41 +0100

    Create client.ts

---
+++ src/lib/supabse/client.ts
@@ -0,0 +1,9 @@
+// src/lib/supabase/client.ts
+import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import type { Database } from './types';
+
+export const createClient = () =>
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
