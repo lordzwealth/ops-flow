37e3d594dbb54c8622ca10e66bf87baf753459bd
Emmanuel Ajayi <ajayiemmanuel215@gmail.com>
2026-02-02 12:09:16 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,13 @@
-// middleware.ts (root)
-import { type NextRequest, NextResponse } from 'next/server';
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  return NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.png|.*\\.svg).*)',
   ],
 };
